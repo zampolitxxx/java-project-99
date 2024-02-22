d11daff65608756a4c9ac9df3baa3444914c6b86
zampolitxxx <zampolitxxx@gmail.com>
2024-02-22 11:46:57 +0900

    remove linter comment
    replace import .* to
    jakarta.persistence.Entity in model.User

--- src/main/java/hexlet/code/app/model/User.java
+++ src/main/java/hexlet/code/app/model/User.java
@@ -1,6 +1,11 @@
 package hexlet.code.app.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
